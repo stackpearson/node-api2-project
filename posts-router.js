2ed7ec552e021aa3dc5810a8a231037242058c22
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-09-06 19:36:15 -0600

    post requests completed

--- posts-router.js
+++ posts-router.js
@@ -22,8 +22,9 @@ router.get('/:id', (req, res) => {
     .then(data => {
         res.status(200).json(data)
     })
-    .catch( error => {
-        res.status(500).json({message: 'id not valid'})
+    .catch( err => {
+        res.status(400).json({ error: "The post information could not be retrieved." })
+        res.status(404).json({message: "The post with the specified ID does not exist."})
     });
 })
 
@@ -33,10 +34,39 @@ router.get('/:id/comments', (req, res) => {
     .then(data => {
         res.status(200).json(data)
     })
-    .catch( error => {
-        res.status(500).json({message: 'id not valid'})
+    .catch( err => {
+        res.status(404).json({message: 'id not valid'})
     })
 })
 
+///----------------------Post Requests---------------------///
+//post new post
+router.post('/', (req, res) => {
+    dataEntries.insert(req.body)
+    .then(entry => {
+        res.status(201).json(entry)
+    })
+    .catch(err => {
+        res.status(400).json({
+            error: 'Please provide title and contens for the post.'
+        })
+        res.status(500).json({
+            error: "There was an error while saving the post to the database"
+        })
+    })
+})
+
+//post comment to post refereced by id
+router.post('/:id/comments', (req, res) => {
+    dataEntries.insertComment(req.body)
+    .then(entry => {
+        res.status(201).json(entry)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "system error"})
+        res.status(404).json({message: "The post with the specified ID does not exist"})
+        res.status(400).json({errorMessage: "please provide text for the comment"})
+    })
+})
 
 module.exports = router;
